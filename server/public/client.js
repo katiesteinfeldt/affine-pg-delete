1d1d9d013f5879251a9ef6d9c1d6ea1b0de6a925
Katie Mangan <katie.mangan1@gmail.com>
2019-02-13 10:10:37 -0600

    capture input values on click

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,13 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery');
+    $('#addRestaurantButton').on('click', addRestaurant);
 }//end onReady 
+
+function addRestaurant() {
+    console.log('addRestaurant being clicked');
+    let restaurantName = $('#restaurantName').val();
+    let restaurantType = $('#restaurantType').val();
+    $('#restaurantName').val('');
+    $('#restaurantType').val('');
+}//end addRestaurant
